293ec3e4753edff5d276f5ee77f33c446d2806b3
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-03-24 08:42:06 -0700

    add backend logic and tests for Member Class

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -13,6 +13,37 @@ public class Member {
     mMemberName = name;
     mSkills = skills;
     mRole = roll;
+    instances.add(this);
     mId = instances.size();
+  }
+
+  public String getMemberName(){
+    return mMemberName;
+  }
+  public String getSkills(){
+    return mSkills;
+  }
+  public String getRole(){
+    return mRole;
+  }
+
+  public static List<Member> all(){
+    return instances;
+  }
+
+  public static void clear(){
+  instances.clear();
+  }
+
+  public int getId(){
+    return mId;
+  }
+
+  public static Member find(int id){
+    try{
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException exception) {
+      return null;
+    }
   }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -8,4 +8,46 @@ public class MemberTest {
   public void Member_instantiatesAsMember_true(){
     assertEquals(true, testMember instanceof Member);
   }
+
+  @Test
+  public void Member_instantiatesWithName_nameString(){
+    assertEquals("Stormi Hoebe", testMember.getMemberName());
+  }
+
+  @Test
+  public void Member_instantiatesWithSkills_skillsString(){
+    assertEquals("Java, JavaScript, UX Design", testMember.getSkills());
+  }
+
+  @Test
+  public void Member_instantiatesWithRole_roleString(){
+    assertEquals("Developer", testMember.getRole());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfMember_true(){
+    Member testMember2 = new Member("Test Name", "Test Skill Set", "Test Roll");
+    assertEquals(true, Member.all().contains(testMember));
+    assertEquals(true, Member.all().contains(testMember2));
+  }
+
+  @Test
+  public void clear_emptiesAllInstancesofMemberfromArrayList_true() {
+    Member testMember2 = new Member("Test Name", "Test Skill Set", "Test Roll");    Member.clear();
+    assertEquals(0, Member.all().size());
+  }
+
+  @Test
+  public void getId_memberInstantiatesWithID_1(){
+    Member.clear();
+    Member testMember2 = new Member("Test Name", "Test Skill Set", "Test Roll");
+    assertEquals(1, testMember2.getId());
+  }
+
+  @Test
+  public void find_returnsMemberWithSameID_testMember() {
+    Member.clear();
+    Member testMember = new Member("Test Name", "Test Skill Set", "Test Roll");
+    assertEquals(Member.find(testMember.getId()), testMember);
+  }
 }

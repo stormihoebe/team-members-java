e66f5576d2cf87156835ea2a519c426c1ee314cb
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-03-24 09:25:55 -0700

    add initial info to app.java including stock teams and members

---
+++ src/main/java/App.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.ArrayList;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    //Default Teams
+
+    Team teamRocket = new Team("Team Rocket", "The Moon", "Terraform Mars");
+    Member jessie = new Member("Jessie", "Rocket Science, Engineering", "Group Lead");
+    Member james = new Member ("James", "Space Enthusiast, Robotics Engineer", "Engineering Coordinator");
+    teamRocket.addMember(jessie);
+    teamRocket.addMember(james);
+
+    Team avengers = new Team("The Avengers", "USA", "Protect earth from threats");
+    Member cap = new Member("Captain America", "American History, Super Strength", "Group Lead");
+    Member iron = new Member("Iron Man", "Weapons Engineering, Electricity Production", "Weapons Developer");
+    Member widow = new Member("Black Widow", "Espionage, Ninja Skills", "Project Manager");
+    avengers.addMember(cap);
+    avengers.addMember(iron);
+    avengers.addMember(widow);
+  }
+}
